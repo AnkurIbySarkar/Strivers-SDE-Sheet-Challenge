749df9bd2b5c4a9b878d53e17b40625fd001f912
Ankur-Sarkar <ssarkqr@gmail.com>
2023-06-08 17:40:44 +0530

    Question 3

---
+++ nextpermutation.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+vector<int> nextPermutation(vector<int> &permutation, int n)
+{
+    //  Write your code here.
+
+    // Find the next lexicographically greater permutation
+    next_permutation(permutation.begin(), permutation.end());
+
+    return permutation;
+}
+vector<int> nextPermutation(vector<int> &permutation, int n)
+{
+    //  Write your code here.
+    int index = -1;
+    for (int i = n - 2; i >= 0; i--)
+    {
+        if (permutation[i] < permutation[i + 1])
+        {
+            index = i;
+            break;
+        }
+    }
+
+    if (index == -1)
+    {
+        reverse(permutation.begin(), permutation.end());
+        return permutation;
+    }
+
+    for (int i = n - 1; i > index; i--)
+    {
+        if (permutation[i] > permutation[index])
+        {
+            swap(permutation[i], permutation[index]);
+            break;
+        }
+    }
+    reverse(permutation.begin() + index + 1, permutation.end());
+
+    return permutation;
+}
