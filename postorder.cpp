824b4c077efef70315c38f2bcdf5413b9ee52df4
Ankur-Sarkar <ssarkqr@gmail.com>
2023-06-24 15:33:20 +0530

    Question 113

---
+++ postorder.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class TreeNode
+{
+public:
+    int data;
+    TreeNode *left, *right;
+    TreeNode() : data(0), left(NULL), right(NULL) {}
+    TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+};
+void postorder(TreeNode *root, vector<int> &ds)
+{
+    if (root == NULL)
+        return;
+    postorder(root->left, ds);
+    postorder(root->right, ds);
+    ds.push_back(root->data);
+}
+vector<int> getPostOrderTraversal(TreeNode *root)
+{
+    // Write your code here.
+    vector<int> ans;
+    postorder(root, ans);
+    return ans;
+}
