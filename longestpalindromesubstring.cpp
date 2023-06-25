97f482e0f474f2b7f4d36db3d93250bdbf5ea1fc
Ankur-Sarkar <ssarkqr@gmail.com>
2023-06-25 16:31:23 +0530

    Question 100

--- longestpalindromesubstring.cpp
+++ longestpalindromesubstring.cpp
@@ -1,31 +1,29 @@
 #include <iostream>
 using namespace std;
-string check(string &s, int L, int R)
+int check(string &s, int L, int R)
 {
     while (L >= 0 && R < s.size() && s[L] == s[R])
     {
         L--;
         R++;
     }
-    return s.substr(L + 1, R - L - 1);
+    return R - L - 1;
 }
 string longestPalinSubstring(string str)
 {
     // Write your code here.
-    string ans;
+    int ans = 0, st = 0;
     int n = str.size();
     for (int i = 0; i < n; i++)
     {
-        string len1 = check(str, i, i);
-        string len2 = check(str, i, i + 1);
-        if (len1.size() > ans.size())
+        int len1 = check(str, i, i);
+        int len2 = check(str, i, i + 1);
+        int len = max(len1, len2);
+        if (len > ans)
         {
-            ans = len1;
-        }
-        if (len2.size() > ans.size())
-        {
-            ans = len2;
+            ans = len;
+            st = i - (len - 1) / 2;
         }
     }
-    return ans;
+    return str.substr(st, ans);
 }
