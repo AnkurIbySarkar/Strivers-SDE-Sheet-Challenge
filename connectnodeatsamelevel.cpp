b03b3376b0997f129be7b3d3495888c524e21845
Ankur-Sarkar <ssarkqr@gmail.com>
2023-06-29 19:28:15 +0530

    Question 136

---
+++ connectnodeatsamelevel.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+template <typename T>
+class BinaryTreeNode
+{
+public:
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+    BinaryTreeNode<T> *next;
+
+    BinaryTreeNode(T data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+        next = NULL;
+    }
+};
+
+void connectNodes(BinaryTreeNode<int> *root)
+{
+    // Write your code here.
+    if (root == NULL)
+        return;
+    queue<BinaryTreeNode<int> *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        int n = q.size();
+        for (int i = 0; i < n; i++)
+        {
+            BinaryTreeNode<int> *temp = q.front();
+            q.pop();
+            if (i == n - 1)
+                temp->next = NULL;
+            else
+            {
+                temp->next = q.front();
+            }
+            if (temp->left != NULL)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right != NULL)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
