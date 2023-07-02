b50033c6f2c043ad697ec798b17ea6f1377d12c9
Ankur-Sarkar <ssarkqr@gmail.com>
2023-07-02 12:20:16 +0530

    Question 99

---
+++ reversewordsinastr.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+string reverseString(string &str)
+{
+    // Write your code here.
+    // Write your code here.
+    vector<string> words;
+    stringstream ss(str);
+    string tmp;
+    while (ss >> tmp)
+        words.push_back(tmp);
+
+    string ans;
+    for (int i = words.size() - 1; i >= 0; --i)
+    {
+        if (i != words.size() - 1)
+            ans += " ";
+        ans += words[i];
+    }
+    return ans;
+}
